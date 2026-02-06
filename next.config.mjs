4f90a199a7b93f7d81dbec191f05506fda68898e
JobNomad <loko88@naver.com>
2026-02-07 07:26:11 +0900

    Remove experimental.serverActions from config
    Removed unnecessary experimental.serverActions option as it is now enabled by default in Next.js 14.

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
+  // Next.js 14 버전부터 Server Actions가 기본으로 활성화되어 
+  // experimental.serverActions 옵션은 더 이상 필요하지 않으므로 삭제했습니다.
+  experimental: {},
   images: {
     remotePatterns: [
       {
+        protocol: 'https',
         hostname: 'books.google.com'
       },
       {
+        protocol: 'https',
         hostname: 'wikibook.co.kr'
       },
     ]
